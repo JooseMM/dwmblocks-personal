af3f5b7cbd5b7ce5a4a23e540700a126aa066a27
Jose Moreno <jamm.webdev@gmail.com>
2025-05-10 23:54:12 -0400

    before statuscolor

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update 	Signal*/
 	{"   ", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,			0},
 
-	{" 󰥔 ", 	"date '+%b  %d  (%a)  %I:%M%p '",				60,			0},
+	{" 󰥔  ", 	"date '+%b  %d  (%a)  %I:%M%p '",				60,			0},
 
 	{" 󰁹 ", 	"echo $(acpi -b) | grep -oE '[0-9]+%' | head -1",		60,			0},
 };
--- scripts/battery.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-battery_info=$(acpi -b)
-battery_percentage=$(echo "$battery_info" | grep -oE '[0-9]+%' | head -1)
